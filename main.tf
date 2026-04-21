a5a0a427fad0858c684c20d7b8cffcf76505a95c
wellsiau-aws <wellsiau@amazon.com>
2026-04-20 20:04:48 -0700

    test tflint

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
 resource "aws_s3_bucket" "example" {
   bucket = "my-bucket"
-
-  # Reference a resource that doesn't exist — triggers validate error
-  tags = {
-    Name = aws_instance.nonexistent.id
-  }
 }
